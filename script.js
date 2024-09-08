226087788f1c4724a5090fd3a67af9ceb9965710
Gokulavasan Govindan <ggovindan@freshworks.com>
2024-09-08 15:12:21 +0530

    changed string in output

--- script.js
+++ script.js
@@ -113,6 +113,6 @@ function evaluate(input) {
     document.getElementById("output-value").innerText = result;
   } catch (e) {
     console.log(e);
-    document.getElementById("output-value").innerText = "Konjam medhuva sollunga please";
+    document.getElementById("output-value").innerText = "konjam english la medhudva sollunga";
   }
 }
